57bfb00ba23f57638ca76243e9f17645373b61b9
Emile Fokkema <emilefokkema@gmail.com>
2025-09-23 22:44:18 +0200

    prepare to create list of anagrams

--- src/app/anagram-list.ts
+++ src/app/anagram-list.ts
@@ -1,13 +1,55 @@
+import type { VeryLongListData } from "./very-long-list-data"
+
 export type AnagramElements = string[]
+export interface AnagramListItem {
+    elements: AnagramElements
+    permutation: number[]
+}
 
 export interface AnagramList {
     setElements(elements: AnagramElements): Promise<void>
+    getListData(): Promise<VeryLongListData<AnagramListItem>>
 }
 
 export function createAnagramList(): AnagramList {
+    const worker = new Worker('../worker/main.ts');
+    worker.postMessage('blah')
     return {
         setElements(elements) {
             return Promise.resolve();
+        },
+        getListData() {
+            return Promise.resolve({
+                items: {
+                    items: [
+                        {
+                            elements: ['a','n', 'a', 'g', 'r', 'a', 'm'],
+                            permutation: [0, 1, 2, 3, 4, 5, 6]
+                        }
+                    ],
+                    hasPrevious: false,
+                    hasNext: false
+                },
+                getItemsAfterItem(item, nrOfItems) {
+                    return Promise.resolve({
+                        items: [],
+                        hasPrevious: false,
+                        hasNext: false
+                    })
+                },
+                getItemsBeforeItem(item, nrOfItems) {
+                    return Promise.resolve({
+                        items: [],
+                        hasPrevious: false,
+                        hasNext: false
+                    })
+                },
+                renderItem(item) {
+                    const span = document.createElement('span');
+                    span.textContent = item.elements.join('');
+                    return span;
+                },
+            })
         },
     }
 }
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,60 +1,16 @@
+import { createAnagramList } from './anagram-list';
 import './very-long-list';
-import type { VeryLongListData } from './very-long-list-data';
 
-function initialize(): void {
+async function initialize(): Promise<void> {
+    const anagramList = createAnagramList();
     const input = document.querySelector('input')!;
-    const list = document.querySelector('very-long-list')!;
-    const worker = new Worker('../worker/main.ts');
-    worker.postMessage('blah')
+    const veryLongList = document.querySelector('very-long-list')!;
+
     input.addEventListener('input', () => {
         console.log('there is input')
-    })
-    const totalItems = 10**5;
-    const tableData: VeryLongListData<number> = {
-        items: {
-            items: [50000],
-            hasNext: true,
-            hasPrevious: true
-        },
-        async getItemsAfterItem(item, nrOfItems) {
-            const result: number[] = [];
-            let itemsAdded = 0;
-            let currentItem = item;
-            while(itemsAdded < nrOfItems && currentItem <= totalItems - 1){
-                currentItem++;
-                result.push(currentItem);
-                itemsAdded++;
-            }
-            await new Promise<void>((resolve) => setTimeout(resolve, 200));
-            return {
-                items: result,
-                hasNext: currentItem < totalItems,
-                hasPrevious: true
-            };
-        },
-        async getItemsBeforeItem(item, nrOfItems) {
-            const result: number[] = [];
-            let itemsAdded = 0;
-            let currentItem = item;
-            while(itemsAdded < nrOfItems && currentItem >= 1){
-                currentItem--;
-                result.unshift(currentItem);
-                itemsAdded++;
-            }
-            await new Promise<void>((resolve) => setTimeout(resolve, 200));
-            return {
-                items: result,
-                hasNext: true,
-                hasPrevious: currentItem > 0
-            };
-        },
-        renderItem(item) {
-            const span = document.createElement('span');
-            span.textContent = `${item}`;
-            return span;
-        },
-    }
-    list.setData(tableData);
+    });
+    const anagramListData = await anagramList.getListData();
+    veryLongList.setData(anagramListData);
 }
 
 initialize();
