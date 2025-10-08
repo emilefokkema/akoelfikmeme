4d5622b4ccfc553bcc5ebf58efaf8fb88f8c492c
Emile Fokkema <emilefokkema@gmail.com>
2025-10-08 21:13:33 +0200

    add the extra scrollbar thumb

--- src/app/very-long-list-scrollbar.ts
+++ src/app/very-long-list-scrollbar.ts
@@ -3,13 +3,18 @@ export class VeryLongListScrollbar extends HTMLElement {
     private isVisible = false
     private _ratio = 1;
     private _scrolledRatio = 0;
-    private height = 1;
+    private thumb: HTMLElement | undefined;
+    private extraThumbUpperScrollRatioBoundary: number | undefined;
+    private extraThumbLowerScrollRatioBoundary: number | undefined;
+    private isOutsideExtraThumbScrollRatioBoundaries: boolean | undefined;
+    private extaThumb: HTMLElement | undefined;
+    private extraTrackVisible = false;
 
     public get thumbRatio(): number {
         return this._ratio;
     }
     public set thumbRatio(value: number){
-        if(this._ratio === value){
+        if(this._ratio === value || !this.thumb){
             return;
         }
         this._ratio = value;
@@ -17,26 +22,36 @@ export class VeryLongListScrollbar extends HTMLElement {
             return;
         }
         const container = this.shadow.getElementById('container')!;
-        const thumb = this.shadow.getElementById('thumb')!;
-        const {height} = container.getBoundingClientRect();
-        this.height = height;
-        const thumbHeight = height * value;
-        thumb.style.height = `${thumbHeight}px`;
+        const { height } = container.getBoundingClientRect();
+        const thumbHeight = value * 100;
+        const thumbHeightInPixels = height * value;
+        const extraTrackVisible = thumbHeightInPixels <= 1;
+        if(extraTrackVisible){
+            container.classList.add('extra-track-visible');
+            const boundary = 5 / height;
+            this.extraThumbLowerScrollRatioBoundary = boundary;
+            this.extraThumbUpperScrollRatioBoundary = 1 - boundary;
+        }else{
+            container.classList.remove('extra-track-visible');
+        }
+        this.extraTrackVisible = extraTrackVisible;
+        this.thumb.style.height = `${thumbHeight}%`;
+        this.setExtraTrackThumb(0)
     }
     public get scrolledRatio(): number {
         return this._scrolledRatio;
     }
     public set scrolledRatio(value: number){
-        if(this._scrolledRatio === value){
+        if(this._scrolledRatio === value || !this.thumb){
             return;
         }
         this._scrolledRatio = value;
         if(!this.shadow){
             return;
         }
-        const thumb = this.shadow.getElementById('thumb')!;
-        const thumbTop = this.height * value;
-        thumb.style.top = `${thumbTop}px`;
+        const thumbTop = value * 100;
+        this.thumb.style.top = `${thumbTop}%`;
+        this.setExtraTrackThumb(value)
     }
     public get visible(): boolean {
         return this.isVisible;
@@ -53,12 +68,48 @@ export class VeryLongListScrollbar extends HTMLElement {
             container.classList.remove('visible')
         }
     }
+    private setExtraTrackThumb(value: number): void {
+        if(
+            !this.extraTrackVisible ||
+            !this.extaThumb ||
+            this.extraThumbLowerScrollRatioBoundary === undefined ||
+            this.extraThumbUpperScrollRatioBoundary === undefined
+        ){
+            return;
+        }
+        const thumbTop = value * 100;
+        const isOutsideExtraThumbScrollRatioBoundaries = value > this.extraThumbLowerScrollRatioBoundary && value < this.extraThumbUpperScrollRatioBoundary;
+        if(isOutsideExtraThumbScrollRatioBoundaries){
+            if(this.isOutsideExtraThumbScrollRatioBoundaries !== true){
+                this.isOutsideExtraThumbScrollRatioBoundaries = true;
+                this.extaThumb.style.removeProperty('clip-path');
+            }
+            this.extaThumb.style.top = `${thumbTop - 100 * this.extraThumbLowerScrollRatioBoundary}%`
+        }else{
+            const isInLower = value < this.extraThumbLowerScrollRatioBoundary
+            const isInUpper = value > this.extraThumbUpperScrollRatioBoundary;
+            if(this.isOutsideExtraThumbScrollRatioBoundaries){
+                this.isOutsideExtraThumbScrollRatioBoundaries = false;
+                if(isInLower){
+                    this.extaThumb.style.removeProperty('top');
+                }
+            }
+            if(isInLower){
+                this.extaThumb.style.clipPath = `polygon(0 100%, 100% ${(value / this.extraThumbLowerScrollRatioBoundary) * 50}%, 0 0)`
+            }
+            if(isInUpper){
+                this.extaThumb.style.clipPath = `polygon(0 100%, 100% ${(1 + (value - this.extraThumbUpperScrollRatioBoundary) / this.extraThumbLowerScrollRatioBoundary) * 50}%, 0 0)`
+            }
+        }
+    }
     protected connectedCallback(): void {
         const templateEl = document.getElementById('very-long-list-scrollbar-template') as HTMLTemplateElement;
         const content = templateEl.content.cloneNode(true);
         const shadow = this.attachShadow({mode: 'open'});
         shadow.appendChild(content);
         this.shadow = shadow;
+        this.thumb = shadow.getElementById('thumb')!;
+        this.extaThumb = shadow.getElementById('extra-thumb')!;
     }
 }
 
